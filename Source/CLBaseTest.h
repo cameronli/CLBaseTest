3217986c18695e4c0f76cd2243fa675c05bc6ecc
李嘉明 <cameron.li@vipshop.com>
2020-06-13 23:27:58 +0800

    创建工程

---
+++ Source/CLBaseTest.h
@@ -0,0 +1,19 @@
+//
+//  CLBaseTest.h
+//  CLBaseTest
+//
+//  Created by CameronL on 2020/6/13.
+//  Copyright © 2020 CameronL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLBaseTest.
+FOUNDATION_EXPORT double CLBaseTestVersionNumber;
+
+//! Project version string for CLBaseTest.
+FOUNDATION_EXPORT const unsigned char CLBaseTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLBaseTest/PublicHeader.h>
+
+
